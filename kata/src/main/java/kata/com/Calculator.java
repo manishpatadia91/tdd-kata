6ddb46f795dc3c658829bbbdf039fd4080d43734
manishp <manish.patadia91@gmail.com>
2019-07-27 05:22:30 -0400

    test case for kata(2) - refactor code

--- kata/src/main/java/kata/com/Calculator.java
+++ kata/src/main/java/kata/com/Calculator.java
@@ -3,14 +3,23 @@ package kata.com;
 public class Calculator {
 
 	public int add(String input) {
-		if(input.isEmpty()) {
+		if (input.isEmpty()) {
 			return 0;
 		}
 		String[] numbers = input.split(",");
-		if(numbers.length == 1){
-			return Integer.parseInt(numbers[0]);
-		}else {
-			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		if (numbers.length == 1) {
+			return stringToInt(numbers[0]);
+		} else {
+			return sumOfNumbers(numbers[0], numbers[1]);
 		}
 	}
+
+	private int sumOfNumbers(String num1, String num2) {
+		return stringToInt(num1) + stringToInt(num2);
+	}
+
+	private int stringToInt(String str) {
+		return Integer.parseInt(str);
+	}
+
 }
