c9b11f8d93a95f0cdd5b3f72ff6407cc4064e1d2
manishp <manish.patadia91@gmail.com>
2019-07-27 02:05:07 -0400

    Test case for kata(1)

---
+++ kata/src/main/java/kata/com/Calculator.java
@@ -0,0 +1,8 @@
+package kata.com;
+
+public class Calculator {
+
+	public int add(String input) {
+		return -1;
+	}
+}
---
+++ kata/src/test/groovy/kata/com/CalculatorSpec.groovy
@@ -0,0 +1,19 @@
+package kata.com
+
+import spock.lang.Specification
+
+class CalculatorSpec extends Specification{
+	def Calculator cal = new Calculator()
+	def "Test to add numbers"() {
+		given : "input as string"
+			input
+		when : "calc.add(input) called"
+			def output = calc.add(input)
+		then : "It should add numbers provided into input"
+			output == expectedOutput
+		where : "Provided input"
+			input					||	expectedOutput
+			""						||		0
+	}
+		
+}
