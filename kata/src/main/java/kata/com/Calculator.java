ad417eda3537d89df5de1cdd46cbac1def54f0fe
Manish Patadia <manish.patadia91@gmail.com>
2019-07-28 17:21:45 +0530

    test cse for kata(4) -- support different delimiters - pass test case

--- kata/src/main/java/kata/com/Calculator.java
+++ kata/src/main/java/kata/com/Calculator.java
@@ -1,18 +1,28 @@
 package kata.com;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
 	public int add(String input) {
 		if (input.isEmpty()) {
 			return 0;
 		}
-		String[] numbers = splitNumbers(input);
+		String[] numbers = tokenizer(input);
 		return sumOfNumbers(numbers);
 	}
 
-	private String[] splitNumbers(String input) {
-		String[] numbers = input.split(",|\n");
-		return numbers;
+	private String[] tokenizer(String input) {
+		if(input.startsWith("//")) {
+			Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+			m.matches();
+			String customDelimiter = m.group(1);
+			String numbers = m.group(2);
+			return numbers.split(customDelimiter);
+		}
+		String[] tokens = input.split(",|\n");
+		return tokens;
 	}
 
 	private int sumOfNumbers(String[] numbers) {
